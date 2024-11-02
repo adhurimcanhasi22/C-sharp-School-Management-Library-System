e6300f3d021d3c8efcfbc31c0e2b2fc3fac88e36
Adhurim Canhasi <121344747+adhurimcanhasi22@users.noreply.github.com>
2024-11-02 13:44:59 +0100

    Add files via upload

---
+++ SchoolLibrarySystem/LibrarySystem/frmLogin.cs
@@ -0,0 +1,41 @@
+﻿using LibrarySystem.Includes;
+using System;
+using System.Windows.Forms;
+
+namespace LibrarySystem
+{
+    public partial class frmLogin : Form
+    {
+        private Form1 frm;
+
+        public frmLogin(Form1 frm)
+        {
+            InitializeComponent();
+            this.frm = frm;
+        }
+
+        private SQLConfig config = new SQLConfig();
+        private string sql;
+
+        private void frmLogin_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void OK_Click(object sender, EventArgs e)
+        {
+            sql = "SELECT * FROM `tbluser` WHERE User_name= '" + UsernameTextBox.Text + "' and Pass = '" + PasswordTextBox.Text + "'";
+            config.singleResult(sql);
+
+            if (config.dt.Rows.Count > 0)
+            {
+                MessageBox.Show("Welcome to Library System");
+                frm.enabled_menu();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Account does not exist. Please contact administrator.", "Invalid", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
